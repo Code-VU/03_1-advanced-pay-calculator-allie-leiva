aa15941554c915b8b79314ec396a84096da3e928
Allie Leiva <Allie.Leiva@vu.com>
2025-02-10 16:00:09 -0600

    if else statements on ot pay

--- payCalculator.py
+++ payCalculator.py
@@ -1,6 +1,25 @@
 def calculatePay():
     # This first line is provided for you
-    hrs = input("Enter Hours:")
+    hrs = input("Enter Hours: ")
+    hrs = float(hrs)
+
+    hourly_rate = input("Enter Rate: ")
+    hourly_rate = float(hourly_rate)
+
+    if hrs  > 40:
+        #logic that adds overtime
+        overtime_hours = hrs - 40
+        normal_hours = 40
+
+        overtime_pay = overtime_hours * hourly_rate * 1.5
+        normal_pay = normal_hours * hourly_rate
+
+        gross_pay = normal_pay + overtime_pay
+            #the usual
+    else:
+        gross_pay = hourly_rate * hrs
+    
+    print(f"Pay: {gross_pay}")
     
     # end assignment
 
